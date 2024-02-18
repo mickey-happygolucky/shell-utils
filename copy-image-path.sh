deef33ec7c580101de5b4effefedd9d86daa2269
Yusuke Mitsuki <mickey.happygolucky@gmail.com>
2024-02-18 14:05:22 +0900

    Add copy-image-path.sh
    The script copies an path of a wic image using xsel.
    A wic image is searched from a build directory of the YoctoProject.
    
    Signed-off-by: Yusuke Mitsuki <mickey.happygolucky@gmail.com>

---
+++ copy-image-path.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+RUNNER=""
+IMAGE=""
+BUILD_DIR=""
+FTYPE="l"
+
+function RUN() {
+    ${RUNNER} "$@"
+}
+
+function usage_exit() {
+    echo "Usage: $0  [-b build-dir] [-nh] imagename"
+    echo "       -b(--build-dir): build direcotry"
+    echo "       -i(--isar)     : for isar"
+    echo "       -n(--dry-run)  : dry run"
+    echo "       -h(--help)     : help(this message)"
+    exit 0
+}
+
+
+function check_args() {
+    declare args
+    args=$(getopt -o b:inh -l build-dir:,isar,dry-run,help -- "$@") || exit 22
+    eval set -- "$args"
+
+    # loop for parsing
+    while [ $# -gt 0 ]; do
+        case $1 in
+	    -b | --build-dir)
+		BUILD_DIR="$2"
+		shift 2
+		;;
+	    -i | --isar)
+		FTYPE="f"
+		shift
+		;;
+            -n | --dry-run)
+                RUNNER="echo"
+                shift
+                ;;
+            -h | --help)
+                usage_exit
+                shift
+                ;;
+            --)
+                shift
+                break
+                ;;
+        esac
+    done
+
+    IMAGE=$1
+    if [ "${IMAGE}" = "" ]; then
+        usage_exit
+    fi
+
+    echo "IMAGE     = ${IMAGE}"
+    echo "BUILD_DIR = ${BUILD_DIR}"
+}
+
+function find_image_path() {
+    declare -r target_dir="$1"
+    declare found_path=""
+
+    echo "target_dir = ${target_dir}"
+    found_path=$(find "${target_dir}" -type ${FTYPE} -a -name "${IMAGE}*wic.*" -not -name '*.bmap'| head -n1)
+    echo "found_path = ${found_path}"
+    if [ -z "${found_path}" ]; then
+	found_path=$(find "${target_dir}" -name "${IMAGE}*wic" | head -n1)
+    fi
+    if [ -z "${found_path}" ]; then
+	echo "wic not found."
+	return 1
+    fi
+    echo -n "${found_path}" | xsel
+    echo "${found_path} is copied!"
+    return 0
+}
+
+check_args "$@"
+if ! find_image_path "${PWD}/${BUILD_DIR}/tmp/deploy/images"; then
+    find_image_path "${PWD}/${BUILD_DIR}/tmp-glibc/deploy/images"
+fi
